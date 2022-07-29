bd29c4f02291b90dc96f18e08d9c55d0c51fa52a
FxRayHughes <bushuaichen@yeah.net>
2022-07-29 14:59:21 +0800

    物品的ItemConfig

--- project/implementation-v4/src/main/kotlin/ink/ptms/um/impl4/Item4.kt
+++ project/implementation-v4/src/main/kotlin/ink/ptms/um/impl4/Item4.kt
@@ -2,11 +2,24 @@ package ink.ptms.um.impl4
 
 import ink.ptms.um.Item
 import io.lumine.xikage.mythicmobs.items.MythicItem
+import org.bukkit.configuration.ConfigurationSection
+import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.inventory.ItemStack
 
 class Item4(obj: Any) : Item {
 
     val source = obj as MythicItem
+
+    val root by lazy {
+        val yaml = YamlConfiguration()
+        yaml.load(source.config.file)
+        yaml.getConfigurationSection(source.internalName)!!
+    }
+
+
+    override fun getConfig(): ConfigurationSection {
+        return root
+    }
 
     override fun generateItemStack(amount: Int): ItemStack {
         return source.generateItemStack(amount).toBukkit()
