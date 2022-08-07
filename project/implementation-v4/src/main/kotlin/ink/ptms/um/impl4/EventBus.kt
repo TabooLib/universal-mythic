6bb32568a9a0cc74e803f0a904c92d4cc851aa2d
坏黑 <b@skymc.me>
2022-08-07 19:43:31 +0800

    1.0.0-beta-15

--- project/implementation-v4/src/main/kotlin/ink/ptms/um/impl4/EventBus.kt
+++ project/implementation-v4/src/main/kotlin/ink/ptms/um/impl4/EventBus.kt
@@ -6,22 +6,26 @@ import ink.ptms.um.event.MythicReloadEvent
 import io.lumine.xikage.mythicmobs.api.bukkit.events.MythicMobDeathEvent
 import io.lumine.xikage.mythicmobs.api.bukkit.events.MythicMobSpawnEvent
 import io.lumine.xikage.mythicmobs.api.bukkit.events.MythicReloadedEvent
+import taboolib.common.platform.Ghost
 import taboolib.common.platform.event.SubscribeEvent
 
 object EventBus {
 
+    @Ghost
     @SubscribeEvent
     fun onMobDeathEvent(event: MythicMobDeathEvent) {
         val bus = MobDeathEvent(Mob4(event.mob), event.killer, event.drops).fire()
         event.drops = bus.drop
     }
 
+    @Ghost
     @SubscribeEvent
     fun onMobSpawnEvent(event: MythicMobSpawnEvent) {
         val bus = MobSpawnEvent(Mob4(event.mob), event.mobLevel).fire()
         event.mobLevel = bus.level
     }
 
+    @Ghost
     @SubscribeEvent
     fun onMythicReloadEvent(event: MythicReloadedEvent) {
         MythicReloadEvent().fire()
--- project/implementation-v5/src/main/kotlin/ink/ptms/um/impl5/EventBus.kt
+++ project/implementation-v5/src/main/kotlin/ink/ptms/um/impl5/EventBus.kt
@@ -6,22 +6,26 @@ import ink.ptms.um.event.MythicReloadEvent
 import io.lumine.mythic.bukkit.events.MythicMobDeathEvent
 import io.lumine.mythic.bukkit.events.MythicMobSpawnEvent
 import io.lumine.mythic.bukkit.events.MythicReloadedEvent
+import taboolib.common.platform.Ghost
 import taboolib.common.platform.event.SubscribeEvent
 
 object EventBus {
 
+    @Ghost
     @SubscribeEvent
     fun onMobDeathEvent(event: MythicMobDeathEvent) {
         val bus = MobDeathEvent(Mob5(event.mob), event.killer, event.drops).fire()
         event.drops = bus.drop
     }
 
+    @Ghost
     @SubscribeEvent
     fun onMobSpawnEvent(event: MythicMobSpawnEvent) {
         val bus = MobSpawnEvent(Mob5(event.mob), event.mobLevel).fire()
         event.mobLevel = bus.level
     }
 
+    @Ghost
     @SubscribeEvent
     fun onMythicReloadEvent(event: MythicReloadedEvent) {
         MythicReloadEvent().fire()
