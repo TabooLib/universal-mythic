cfb91ad5b5e3e9a3e56e08b7d7aa34327b0628bd
坏黑 <b@skymc.me>
2023-07-03 21:31:19 +0800

    [publish] beta 27

--- project/common/src/main/kotlin/ink/ptms/um/event/MobSkillLoadEvent.kt
+++ project/common/src/main/kotlin/ink/ptms/um/event/MobSkillLoadEvent.kt
@@ -1,14 +1,7 @@
 package ink.ptms.um.event
 
 import ink.ptms.um.skill.SkillConfig
-import ink.ptms.um.skill.SkillMeta
-import ink.ptms.um.skill.SkillResult
 import ink.ptms.um.skill.type.BaseSkill
-import ink.ptms.um.skill.type.EntityTargetSkill
-import org.bukkit.entity.Entity
-import org.bukkit.entity.LivingEntity
-import taboolib.common.platform.Ghost
-import taboolib.common.platform.event.SubscribeEvent
 import taboolib.platform.type.BukkitProxyEvent
 
 /**
@@ -35,20 +28,19 @@ class MobSkillLoadEvent(val skillName: String, val config: SkillConfig): BukkitP
         return this
     }
 
-    @Ghost
-    @SubscribeEvent
-    fun example(event: MobSkillLoadEvent) {
-        if (event.nameIs("damage")) {
-            event.register(object : EntityTargetSkill {
-
-                val value = event.config.getPlaceholderDouble(arrayOf("value"), 0.0)
-
-                override fun cast(meta: SkillMeta, entity: Entity): SkillResult {
-                    if (entity !is LivingEntity) return SkillResult.ERROR
-                    entity.damage(value[meta.caster])
-                    return SkillResult.SUCCESS
-                }
-            })
-        }
-    }
+//    @SubscribeEvent
+//    fun example(event: MobSkillLoadEvent) {
+//        if (event.nameIs("example_damage")) {
+//            event.register(object : EntityTargetSkill {
+//
+//                val value = event.config.getPlaceholderDouble(arrayOf("value"), 0.0)
+//
+//                override fun cast(meta: SkillMeta, entity: Entity): SkillResult {
+//                    if (entity !is LivingEntity) return SkillResult.ERROR
+//                    entity.damage(value[meta.caster])
+//                    return SkillResult.SUCCESS
+//                }
+//            })
+//        }
+//    }
 }
